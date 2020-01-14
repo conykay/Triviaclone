f73b7926e3312b6461201373db1a388f9f4e1d91
ceekay <ceekayconelius@gmail.com>
2020-01-14 07:01:50 +0300

    implemented shake animation when user gets correct

--- app/src/main/java/com/conelius/trivia/MainActivity.java
+++ app/src/main/java/com/conelius/trivia/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -102,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boolean answerIsTrue = questionList.get(currentQuestionIndex).isAnswerTrue();
         int toastMessageId = 0;
         if (answerIsTrue == userChooseCorrect) {
-
+            fadeView();
             toastMessageId = R.string.correct_answer;
 
         }else{
@@ -121,6 +122,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         questionTextView.setText(question);
         questionCounterTextView.setText(currentQuestionIndex + " / "+questionList.size());
 
+    }
+
+    private void fadeView() {
+        final CardView cardView = findViewById(R.id.cardView);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+
+        alphaAnimation.setDuration(350);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     private void shakeAnimation() {
